bf81d08a800045c0a3d2207c051faba582a25c4b
sunder2568223 <sunderyssr@gmail.com>
2023-08-23 17:18:37 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9ass9ADO.NET_StoredProcedure
+{
+    internal class Program
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static string conStr = "server = LAPTOP-N8306ABM; database = SetupDB; trusted_connection = true;";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                con = new SqlConnection(conStr);
+                con.Open();
+
+                cmd = new SqlCommand()
+                {
+                    CommandText = "PlaceOrder",
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = con
+                };
+
+                Console.Write("Enter Customer ID : ");
+                cmd.Parameters.AddWithValue("@customerId", int.Parse(Console.ReadLine()));
+                Console.Write("Enter Total Amount : ");
+                cmd.Parameters.AddWithValue("@totalAmount", double.Parse(Console.ReadLine()));
+
+                int output = (int)cmd.ExecuteScalar();
+
+                if (output >= 1)
+                {
+                    Console.WriteLine("Order Placed Successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Order Placing Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+    
